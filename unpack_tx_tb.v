0c7ce73d4d4a7d05449a2bab3ccdbd4392dddca8
arjunsankr <arjunsankarop@gmail.com>
2026-03-29 19:37:37 +0530

    Add start signal to unpack_tx testbench

--- unpack_tx_tb.v
+++ unpack_tx_tb.v
@@ -4,10 +4,11 @@
     reg clk;
     reg rstn;
     reg [7:0]din;
+    reg start;
     reg [1:0]datawidthsel;
     wire dout;
 
-    unpack_tx DUT(clk,rstn,din,datawidthsel,dout);
+    unpack_tx DUT(clk,rstn,start,din,datawidthsel,dout);
 
     //clock generation
     initial 
@@ -29,12 +30,14 @@
       begin
         rstn=1'b0;
         #10 rstn=1'b1;
+        start=1'b1;
         din=8'b10110111;
+        #10 start=1'b0;
         datawidthsel=2'b00;
         #1000 $finish;
       end
     always @(posedge clk)begin
-      $display("Time=%0t | rstn=%b | din=%b | datawidthsel=%b | dout=%b", 
-           $time, rstn, din, datawidthsel, dout);
+      $display("Time=%0t | rstn=%b | start | din=%b | datawidthsel=%b | dout=%b", 
+           $time, rstn,start, din, datawidthsel, dout);
     end
   endmodule
